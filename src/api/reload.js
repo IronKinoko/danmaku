fe3ed95263c242bcd5f5acf41c1530b606dfb76f
IronKinoko <kinoko_main@outlook.com>
2022-02-27 17:32:42 +0800

    feat: add reload api

--- src/api/reload.js
+++ src/api/reload.js
@@ -1,6 +1,14 @@
+import { formatMode } from "../utils"
+
 /* eslint-disable no-invalid-this */
 export default function (comments) {
   this.clear()
-  this.comments = comments
+  this.comments = comments || []
+  this.comments.sort(function (a, b) {
+    return a.time - b.time
+  })
+  for (var i = 0; i < this.comments.length; i++) {
+    this.comments[i].mode = formatMode(this.comments[i].mode)
+  }
   return this
 }
